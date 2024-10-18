dc8c1073fa784d8483fd9f8f5b01062d0b783711
cliff-4 <adityamishra0700@gmail.com>
2024-10-19 01:49:47 +0530

    Frontend - init

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+    return (
+        <h1
+            className="
+              w-screen
+              text-3xl font-bold underline
+              border-red-500 border-4 rounded-lg
+              text-center
+            "
+        >
+            Hello world!
+        </h1>
+    );
+}
