505c838580b2a0ddee1af2b1c94a265132f18158
cliff-4 <adityamishra0700@gmail.com>
2024-10-22 05:28:38 +0530

    [feat] logging, cors middleware, /contact, /about

--- Backend/connections.py
+++ Backend/connections.py
@@ -1,41 +1,48 @@
-from fastapi import FastAPI, HTTPException, Path
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import psycopg2
+from dotenv import load_dotenv
+import logging
 
 # from typing import Optional, List, Tuple
 # from enum import Enum
 # from pydantic import BaseModel
-
-import psycopg2
-import os
-from dotenv import load_dotenv
-
 # import datetime
 
+logger = logging.getLogger("uvicorn.error")
 load_dotenv()
 
-conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASS"),
-    port=os.getenv("DB_PORT"),
-)
-curr = conn.cursor()
-
-
-# class Project(BaseModel):
-#     id: int
-#     title: str
-#     short_desc: str
-#     long_desc: str
-#     last_edited: datetime.date
-
+try:
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        port=os.getenv("DB_PORT"),
+    )
+    curr = conn.cursor()
+except Exception as e:
+    logger.error(
+        f"Cound not connect to {os.getenv('DB_HOST')}. Received the following error: {str(e).strip()}"
+    )
+    curr = None
 
 app = FastAPI()
+
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://172.19.0.1:5173"],  # Adjust frontend url
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def index():
-    return {"about": "blank"}
+    return "fuck off :)"
 
 
 @app.get("/project/{id}")
@@ -48,7 +55,25 @@ def get_project(id: int):
     return res
 
 
-@app.get("/projects/")
+@app.get("/projects")
 def get_all_projects():
     curr.execute("SELECT * FROM projects")
     return curr.fetchall()
+
+
+@app.get("/contact")
+def get_contact():
+    curr.execute("SELECT data FROM details WHERE attribute = 'contact'")
+    return curr.fetchone()[0]
+
+
+@app.get("/about")
+async def get_about():
+    if curr:
+        curr.execute("SELECT data FROM details WHERE attribute = 'homepage'")
+        return curr.fetchone()[0]
+    else:
+        logger.warning(
+            f"Cound not connect to {os.getenv('DB_HOST')}. Using default data"
+        )
+        return {}
