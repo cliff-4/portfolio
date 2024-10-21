f68425da13022ee17a1e8dc68a3be23c1573a0b7
cliff-4 <adityamishra0700@gmail.com>
2024-10-21 23:19:50 +0530

    this is the final form

--- Backend/connections.py
+++ Backend/connections.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, HTTPException, Path
-from typing import Optional, List, Tuple
-from enum import Enum
-from pydantic import BaseModel
+
+# from typing import Optional, List, Tuple
+# from enum import Enum
+# from pydantic import BaseModel
 
 import psycopg2
 import os
 from dotenv import load_dotenv
-import json
-import datetime
+
+# import datetime
 
 load_dotenv()
 
@@ -21,12 +22,12 @@ conn = psycopg2.connect(
 curr = conn.cursor()
 
 
-class Project(BaseModel):
-    id: int
-    title: str
-    short_desc: str
-    long_desc: str
-    last_edited: datetime.date
+# class Project(BaseModel):
+#     id: int
+#     title: str
+#     short_desc: str
+#     long_desc: str
+#     last_edited: datetime.date
 
 
 app = FastAPI()
@@ -40,20 +41,14 @@ def index():
 @app.get("/project/{id}")
 def get_project(id: int):
     curr.execute(
-        "SELECT title, short_desc, image, last_edited FROM projects WHERE project_id=%s;",
+        "SELECT * FROM projects WHERE project_id=%s;",
         (id,),
     )
     res = curr.fetchone()
-    # return res
-    return Project(
-        title=res[0],
-        short_desc=res[1],
-        image=res[2],
-        last_edited=res[3],
-    )
+    return res
 
 
 @app.get("/projects/")
 def get_all_projects():
     curr.execute("SELECT * FROM projects")
-    return [Project(*x) for x in curr.fetchall()]
+    return curr.fetchall()
