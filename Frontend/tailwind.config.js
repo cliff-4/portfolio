2dc349b4ddf69064e9a93c2182af338c889a279a
cliff-4 <adityamishra0700@gmail.com>
2024-10-22 05:31:05 +0530

    title, tailwind themes

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,6 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'theme1': '#aed3e9',
+      'theme2': '#b5f1fe',
+      'theme3': '#fffdfd',
+      'theme4': '#91e3d5',
+      'theme5': '#cdeeec',
+      'theme6': '#005bb1',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
