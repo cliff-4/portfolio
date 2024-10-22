0de3ddb4d912b1b6fd6ac4bbe6fe2f52b03a1902
cliff-4 <adityamishra0700@gmail.com>
2024-10-23 03:12:10 +0530

    Project: init

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Home from "./Home";
 import Contacts from "./Contact";
+import Projects from "./Projects";
 
 const Pages = Object.freeze({
-    size: 2,
-    allPages: ["Home", "Contact"],
+    allPages: ["Home", "Projects", "Contact"],
     HOME: 0,
-    CONTACT: 1,
+    PROJECTS: 1,
+    CONTACT: 2,
 });
 
 const currPage = (page: number) => {
@@ -15,6 +16,8 @@ const currPage = (page: number) => {
             return <Home />;
         case Pages.CONTACT:
             return <Contacts />;
+        case Pages.PROJECTS:
+            return <Projects />;
         default:
             return <h1 className="text-white">An error occured... :(</h1>;
     }
@@ -22,23 +25,21 @@ const currPage = (page: number) => {
 
 export default function App() {
     const [page, setPage] = useState(0);
-    const [buttonName, setButtonName] = useState(Pages.allPages[1]);
     return (
         <>
             <button
                 className="
                 absolute bottom-10 left-10
+                min-w-24 max-w-24
                 inline-flex items-center justify-center overflow-hidden
                 bg-theme6 hover:text-white transition-all
                 font-mono
                 "
                 onClick={() => {
-                    setButtonName(Pages.allPages[page % Pages.allPages.length]);
-                    console.log(`Page name set to ${buttonName}`);
-                    setPage((page + 1) % Pages.size);
+                    setPage((page + 1) % Pages.allPages.length);
                 }}
             >
-                {buttonName}
+                {Pages.allPages[(page + 1) % Pages.allPages.length]}
             </button>
             {currPage(page)}
         </>
