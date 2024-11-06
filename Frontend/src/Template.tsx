e74ce2170f70e695439a7670e0bc5af8c1a5c0f5
cliff-4 <adityamishra0700@gmail.com>
2024-11-06 09:50:38 +0530

    misc

--- Frontend/src/Template.tsx
+++ Frontend/src/Template.tsx
@@ -9,7 +9,7 @@ const Window: React.FC<WindowProps> = ({ children }) => {
         <div className="flex items-center justify-center h-screen w-screen bg-theme3 p-5">
             <div
                 className="
-                    w-full h-full p-10
+                    w-full h-full lg:p-10
                     bg-theme3 rounded-lg
                     flex items-center justify-center space-x-10
                     shadow-[rgba(0,0,15,0.3)_5px_5px_10px_5px]
