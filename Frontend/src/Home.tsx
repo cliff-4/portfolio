788debfcf2b6c11d975e5fca7a76c65a8c9e89d1
cliff-4 <adityamishra0700@gmail.com>
2024-11-06 09:55:00 +0530

    - mobile adaptivity - removed window

--- Frontend/src/Home.tsx
+++ Frontend/src/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Window from "./Template";
 import aboutJsonLocal from "../data/about.json";
 
 const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
@@ -8,6 +7,29 @@ interface AboutInfo {
     about: string;
     image: string;
 }
+
+interface ImogeProps {
+    image?: string;
+    className?: string;
+}
+
+const Imoge = ({ image, className }: ImogeProps) => {
+    return (
+        <div className={className}>
+            <img
+                className="
+            w-full h-full
+            bg-cover bg-bottom
+            bg-clip-content
+            rounded-lg
+            object-cover overflow-clip
+            "
+                src={image}
+                alt=""
+            />
+        </div>
+    );
+};
 
 export default function Home() {
     const [about, setAbout] = useState<string>();
@@ -33,39 +55,33 @@ export default function Home() {
     }, []);
 
     return (
-        <Window>
-            <div className="w-1/2 h-full flex flex-col">
+        <div className="w-full h-full flex flex-col lg:flex-row overflow-scroll p-2 gap-2 pb-24">
+            <div className="w-full lg:w-1/2 lg:h-full flex flex-col gap-2">
                 <h1 className="animate-pulse text-theme6 font-mono flex-none">
                     Who_Am_I_?
                 </h1>
-                <span className="max-h-[30rem]">
+                <Imoge image={image} className="lg:absolute lg:hidden" />
+                <span className="lg:max-h-[30rem]">
                     <p
                         className="
-                        max-h-full
-                        block
-                            p-5
-                            text-justify
-                            text-xl
-                            rounded-lg
-                            border-4 border-theme6
-                            overflow-y-auto box-border
-                        "
+                            lg:max-h-full
+                            block
+                                p-5
+                                text-justify
+                                text-xl
+                                rounded-lg
+                                border-4 border-theme6
+                                lg:overflow-y-auto box-border
+                            "
                     >
                         {about}
                     </p>
                 </span>
             </div>
-            <img
-                className="
-                    w-1/2 h-full
-                    bg-cover bg-bottom
-                    bg-clip-content
-                    rounded-lg
-                    object-cover
-                    "
-                src={image}
-                alt=""
+            <Imoge
+                image={image}
+                className="hidden lg:block w-1/2 h-full flex-col justify-center"
             />
-        </Window>
+        </div>
     );
 }
